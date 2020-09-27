63dfd825662435099e59ee9b6547b805224d750f
Javier Pérez Arias <javier@iMac-de-Javier.home>
2020-09-27 18:37:41 +0200

    Added Device plugin to get device specs.

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,19 @@
       <p>Latitude: {{loc.coords.latitude}}</p>
       <p>Longitude: {{loc.coords.longitude}}</p>
     </div>
-
     <button @click="getCurrentPosition">
       Get Current Location
+    </button>
+
+    <h1>Device Info</h1>
+    <div v-if="deviceInfo">
+      <p>OS: {{deviceInfo.platform}} {{deviceInfo.osVersion}}</p>
+      <p>Model: {{deviceInfo.model}}</p>
+      <p>Manufacturer: {{deviceInfo.manufacturer}}</p>
+      <p>UUID: {{deviceInfo.uuid}}</p>
+    </div>
+    <button @click="getDeviceInfo">
+      Get Device Info
     </button>
   </div>
 </template>
@@ -18,13 +28,19 @@ import { Plugins } from '@capacitor/core'
 export default {
   name: 'App',
   data() {
-    return{ loc: null }
+    return{
+      loc: null,
+      deviceInfo: null
+    }
   },
   methods: {
-    async getCurrentPosition(){
+    async getCurrentPosition () {
       const { Geolocation } = Plugins;
-      const loc = await Geolocation.getCurrentPosition()
-      this.loc = loc
+      this.loc = await Geolocation.getCurrentPosition()
+    },
+    async getDeviceInfo () {
+      const { Device } = Plugins
+      this.deviceInfo = await Device.getInfo()
     }
   }
 }
